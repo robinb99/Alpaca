15de64c764533b6331cb6d9d8a2de9873a1207b8
robinb99 <robinvanbeek99@gmail.com>
2025-12-14 22:49:57 +0100

    minor improvements and comments

--- alpaca.py
+++ alpaca.py
@@ -6,8 +6,9 @@ class CodeBlock:
     Generates and executes code based on the provided prompt.
     """
 
-    def __init__(self, model="llama3.1"):
+    def __init__(self, model="llama3.1", run_count=1):
         self.model = model
+        self.run_count = run_count
 
     def generate_and_execute(self, prompt):
         self.prompt = prompt
@@ -15,17 +16,25 @@ class CodeBlock:
             model=self.model,
             messages=[{"role": "user", "content": prompt}],
         )
-
-        print(f"Running task: {self.summarize(self.prompt)}")
+        
+        # show current task as running
+        print(f"Running task {self.show_status_loading()}: {self.summarize(self.prompt)}")
 
         try:
-            
             code = response["message"]["content"]
             code = re.sub(r"^```[\w]*\n|```$", "", code.strip())
             exec(code)
+            
         except Exception as e:
             print(f"Error executing code: {e}")
 
+    # print current run count and increment it
+    def show_status_loading(self):
+        run_count_old = self.run_count
+        self.run_count += 1
+        return run_count_old
+
+    # provide brief summarization of current executed task
     def summarize(self, prompt):
         response = chat(
             model=self.model,
@@ -45,9 +54,10 @@ class Orchestrator:
         self.codeblock = CodeBlock(model=self.model)
 
     def run(self):
-        print("Executing Code...")
-        self.codeblock.generate_and_execute(self.prompt)
-        print("Execution Complete.")
+        for i in range(3):
+            print("Executing Code...")
+            self.codeblock.generate_and_execute(self.prompt)
+            print("Execution Complete.")
 
 if __name__ == "__main__":
     orchestrator = Orchestrator(
