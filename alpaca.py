073c0ab376afe93ae89850210af69293c5566b5b
robinb99 <robinvanbeek99@gmail.com>
2025-12-14 20:32:00 +0100

    OOP initialization

--- alpaca.py
+++ alpaca.py
@@ -1,18 +1,34 @@
 from ollama import chat
 import re
 
-response = chat(
-    model="llama3.1",
-    messages=[
-        {
-            "role": "user",
-            "content": "Write a Python hello world script. Only provide the raw code.",
-        }
-    ],
-)
+class CodeBlock:
+    """"
+    Generates code block based on the provided prompt using the specified model.
+    """
+
+    def __init__(self, prompt, model="llama3.1"):
+        self.model=model
+        self.prompt=prompt
+        response = chat(
+        model=self.model,
+        messages=[
+            {
+                "role": "user",
+                "content": self.prompt,
+            }
+        ],
+    )
+        try:
+            code = response["message"]["content"]
+            code = re.sub(r"^```[\w]*\n|```$", "", code.strip())
+            print(code)
+            return exec(code)
+        except Exception as e:
+            print(f"Error executing code: {e}")
+
+flow = CodeBlock(
+    prompt="""Generate a Python Hello World script. Only provide the raw code.""",
+    model="llama3.1"
+    )
 
 
-code = response["message"]["content"]
-code = re.sub(r"^```[\w]*\n|```$", "", code.strip())
-
-exec(code)
